0be2d8bab015fdb7401859d5807dc4f8941458ea
Matttteo <baiyunhan.cs@gmail.com>
2016-07-19 17:09:46 +0800

    init

---
+++ GetInconsistanceData/Client.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+// https://www.nuget.org/packages/Microsoft.TeamFoundationServer.Client/
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.InteractiveClient/
+using Microsoft.VisualStudio.Services.Client;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.Client/
+using Microsoft.VisualStudio.Services.Common;
+namespace GetInconsistanceData
+{
+    class Client
+    {
+        private static string siteUrl;
+        private static string teamProjectName;
+        private static string uName;
+        private static string pSW;
+        public Client(string url, string tname, string uname, string pw)
+        {
+            siteUrl = url;
+            teamProjectName = tname;
+            uName = uname;
+            pSW = pw;
+        }
+
+        public void BasicAuthRestSample()
+        {
+            // Create instance of VssConnection using basic auth credentials. 
+            // For security, ensure you are connecting to an https server, since credentials get sent in plain text.
+            VssConnection connection = new VssConnection(new Uri(siteUrl), new VssCredentials(new WindowsCredential(new NetworkCredential(uName, pSW))));
+
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+            List<QueryHierarchyItem> items = witClient.GetQueriesAsync(teamProjectName).Result;
+        }
+        public  void SampleREST()
+        {
+            // Create a connection object, which we will use to get httpclient objects.  This is more robust
+            // then newing up httpclient objects directly.  Be sure to send in the full collection uri.
+            // For example:  http://myserver:8080/tfs/defaultcollection
+            // We are using default VssCredentials which uses NTLM against a Team Foundation Server.  See additional provided
+            // examples for creating credentials for other types of authentication.
+            VssConnection connection = new VssConnection(new Uri(siteUrl), new VssCredentials(new WindowsCredential(new NetworkCredential(uName, pSW))));
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            // Get 2 levels of query hierarchy items
+            List<QueryHierarchyItem> queryHierarchyItems = witClient.GetQueriesAsync(teamProjectName, depth: 2).Result;
+
+            // Search for 'My Queries' folder
+            QueryHierarchyItem myQueriesFolder = queryHierarchyItems.FirstOrDefault(qhi => qhi.Name.Equals("My Queries"));
+            if (myQueriesFolder != null)
+            {
+                string queryName = "queryInconsistant";
+
+                // See if our 'REST Sample' query already exists under 'My Queries' folder.
+                QueryHierarchyItem newBugsQuery = null;
+                if (myQueriesFolder.Children != null)
+                {
+                    newBugsQuery = myQueriesFolder.Children.FirstOrDefault(qhi => qhi.Name.Equals(queryName));
+                }
+                if (newBugsQuery == null)
+                {
+                    // if the 'REST Sample' query does not exist, create it.
+                    newBugsQuery = new QueryHierarchyItem()
+                    {
+                        Name = queryName,
+                        Wiql = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State],[System.Tags] FROM WorkItems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Bug' AND [System.State] = 'New'",
+                        IsFolder = false
+                    };
+                    newBugsQuery = witClient.CreateQueryAsync(newBugsQuery, teamProjectName, myQueriesFolder.Name).Result;
+                }
+
+                // run the 'REST Sample' query
+                WorkItemQueryResult result = witClient.QueryByIdAsync(newBugsQuery.Id).Result;
+
+                if (result.WorkItems.Any())
+                {
+                    int skip = 0;
+                    const int batchSize = 100;
+                    IEnumerable<WorkItemReference> workItemRefs;
+                    do
+                    {
+                        workItemRefs = result.WorkItems.Skip(skip).Take(batchSize);
+                        if (workItemRefs.Any())
+                        {
+                            // get details for each work item in the batch
+                            List<WorkItem> workItems = witClient.GetWorkItemsAsync(workItemRefs.Select(wir => wir.Id)).Result;
+                            foreach (WorkItem workItem in workItems)
+                            {
+                               Console.WriteLine("{0}", workItem.Id);
+                            }
+                        }
+                        skip += batchSize;
+                    }
+                    while (workItemRefs.Count() == batchSize);
+                }
+                else
+                {
+                    Console.WriteLine("No work items were returned from query.");
+                }
+            }
+        }
+    }
+}
---
+++ GetInconsistanceData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetInconsistanceData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://protodoc.visualstudio.com/defaultcollection";
+            string name = "protodoc";
+            string uname = "t-yubai@microsoft.com";
+            string psw = "7grcdbkdbxr2fg5y3nwlcpksqjdqelmxz2rlauuo5cr5os7yrk4q";
+            Client c = new Client(url, name, uname, psw);
+            c.SampleREST();
+            return;
+        }
+    }
+}
